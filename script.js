5e9c27ab3a948b927d6a65974e5831ddde38f954
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-06-02 19:54:46 +0000

    decluttered code

--- script.js
+++ script.js
@@ -10,13 +10,11 @@ async function getRandomUser() {
   // fetch random user and add money
   const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
-  
   const user = data.results[0];
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() *1000000)
   }
-
   addData(newUser);
 }
 
@@ -26,19 +24,7 @@ function addData(obj) {
 }
 
 function doubleMoney(){
-  // TODO: Clean up unused code!
-  // providedData.forEach( item => {
-  //   item.money *= 2;
-  //   console.log(item.money);
-  // })
-  // for (let i = 0; providedData === null; i++){
-  //   item.money *= 2;
-  //   console.log(item.money);
-  // }
   data.map(person => {person.money *= 2});
-  //console.log(toMap);
-  //data = toMap;
-  // while (
   updateDOM();
 }
 
@@ -53,8 +39,6 @@ function sortByRichest(){
 
 /* Useful Source: https://bobbyhadz.com/blog/javascript-filter-array-of-objects-based-on-property */
 function showMillionaires(){
-  // TODO: CLEAN UP!
-  // data = data.filter(checkMillionaire);
   const elites = data.filter(data => {
     return data.money > 1000000;
   });
@@ -62,11 +46,6 @@ function showMillionaires(){
   console.log(data);
   updateDOM();
 }
-
-// function checkMillionaire(){
-//   return data.money > 1000000;
-// }
-
 function updateDOM(providedData = data) {
   // Clearing the main element
   main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
@@ -84,22 +63,12 @@ function formatMoney(number) {
 
 // Useful Source: https://stackoverflow.com/questions/5732043/how-to-call-reduce-on-an-array-of-objects-to-sum-their-properties
 function calculateWealth(){
-  // const toReduce = (accumulator, currentValue) => {accumulator.money + currentValue.money};
-  // const toReduce = (function(a, b){
-  //   return {money: a.money + b.money};
-  // });
   const total = data.reduce((a, b) => ({money: a.money + b.money}));
   console.log(total);
   const plus = document.createElement('div');
-  // plus.setAttribute('money', 'div');
-  // plus.classList.add('button');
   plus.innerHTML = `<h3><strong>Total wealth:</strong> ${formatMoney(total.money)}</h3>`;
   main.appendChild(plus);
 }
-
-// function getSum(num){
-//   num += data.money;
-// }
 
 // Event listeners
 addUserBtn.addEventListener('click', getRandomUser);
